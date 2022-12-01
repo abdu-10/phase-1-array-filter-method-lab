0a9a02fb19348655a0bc108f952095430f7bd1de
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-12-01 22:18:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+let findMatching = (ace,block) => ace.filter(ace => ace.toLowerCase() === block.toLowerCase())
+
+let fuzzyMatch = (ace,block) => ace.filter(ace => ace.charAt(0) === block.charAt(0))
+
+let matchName = (ace,block) => ace.filter(ace => ace.name === block)
+
+
+
+
+
+
+
+
+
+
+
+
